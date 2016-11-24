8624bed7a3397a036dd3b32dc889d1aae99e24d4
Roy Curtis <roy.adrian.curtis+github@gmail.com>
2016-11-24 12:28:03 +0000

    Fix config not reloading by reload command

--- src/main/java/me/rafaskb/ticketmaster/commands/CommandReload.java
+++ src/main/java/me/rafaskb/ticketmaster/commands/CommandReload.java
@@ -1,5 +1,6 @@
 package me.rafaskb.ticketmaster.commands;
 
+import me.rafaskb.ticketmaster.utils.ConfigLoader;
 import org.bukkit.command.CommandSender;
 
 import me.rafaskb.ticketmaster.utils.Lang;
@@ -15,6 +16,7 @@ public class CommandReload extends Command {
 	@Override
 	protected void run(CommandSender sender, String[] args) {
 		LangConfig.reloadConfig();
+		ConfigLoader.reloadConfig();
 		Lang.sendMessage(sender, Lang.RELOAD_MESSAGE);
 	}
 	
